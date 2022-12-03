6c4c26eaf61439973b1ee08cf9a81f7ec88e6fbe
Juan Pablo Lagos Moreno <juan.lagos.moreno91@gmail.com>
2022-12-03 14:11:23 -0300

    correcciones en el server.js

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', router)
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
+
     app.use(express.static('frontend/build'))
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+  
+      res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+  
     })
-}
+  
+  }
 
 const port = process.env.PORT || 4000
 
