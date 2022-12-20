2024ea03ab8fccc99359efe054372dc9e710c63c
Juan Pablo Lagos Moreno <juan.lagos.moreno91@gmail.com>
2022-12-19 21:36:35 -0300

    agregando readME

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', router)
 
+if (process.env.NODE_ENV === 'production') {
 
-  // app.use(express.static('frontend/build'))
+    app.use(express.static('frontend/build'))
   
-  // app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
   
-  // res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+      res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
   
-  //   })
+    })
   
-  
+  }
 
-const port = 8080
+const port = process.env.PORT || 4000
 
 app.listen(port, '0.0.0.0',() => console.log('Listening on port 4000'))
